4e5d88b2248d80313dec1ac4e7e17166d2e55a7a
Adam Wolf <adamjwolf1@gmail.com>
2019-02-15 15:35:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
-appendKitten("Ralph")// Add your functions and code here
+destructivelyAppendKitten("Ralph")// Add your functions and code here
